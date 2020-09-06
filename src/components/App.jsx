9a1cbd75adae863124b72442d2bf3889f8254d6b
DustinMcMullen <51675499+DustinMcMullen@users.noreply.github.com>
2020-09-06 15:01:59 -0500

    It woks!!! Added functionality. Notes can be created & deleted (and of course displayed from the array)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function deleteNote(idOfNoteToDelete) {
+    setNotes((prevValue) => {
+      return notes.filter(function (notes, index) {
+        return index !== idOfNoteToDelete;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea setNewNote={setNotes} notes={notes} />
+      {notes.map((eachNote, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={eachNote.title}
+          content={eachNote.content}
+          deleteNote={deleteNote}
+        />
+      ))}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [noteTitle, setNoteTitle] = useState("");
+  const [noteContent, setNoteContent] = useState("");
+
+  function handleInput(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    if (name === "title") {
+      setNoteTitle(value);
+    } else if (name === "content") {
+      setNoteContent(value);
+      // } else if (name === "createNote") {
+    }
+  }
+
+  function submitNewNote(event) {
+    event.preventDefault();
+    props.setNewNote((prevValue) => {
+      return [
+        ...prevValue,
+        {
+          title: noteTitle,
+          content: noteContent
+        }
+      ];
+    });
+    setNoteTitle("");
+    setNoteContent("");
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          name="title"
+          onChange={handleInput}
+          placeholder="Title"
+          value={noteTitle}
+        />
+        <textarea
+          name="content"
+          onChange={handleInput}
+          placeholder="Take a note..."
+          rows="3"
+          value={noteContent}
+        />
+        <button onClick={submitNewNote} name="createNote">
+          Add
+        </button>
       </form>
     </div>
   );
